78523ec92fb2a96740f61a1fed8d68d778db2dec
Paul Kirth <paulkirth@google.com>
2025-12-03 16:37:11 -0800

    Update invoke test for tighter lifetimes

--- clang/test/CodeGenCXX/aggregate-lifetime-invoke.cpp
+++ clang/test/CodeGenCXX/aggregate-lifetime-invoke.cpp
@@ -15,29 +15,27 @@ void test() {
 
   // CHECK: call void @llvm.lifetime.start.p0(ptr{{.*}} %[[AGG1]])
   // CHECK: invoke void @func_that_throws(ptr{{.*}} %[[AGG1]])
-  // CHECK-NEXT: to label %[[CONT1:.*]] unwind label %[[LPAD1:.*]]
+  // CHECK-NEXT: to label %[[CONT1:.*]] unwind label %[[LPAD:.*]]
 
   // CHECK: [[CONT1]]:
+
+  // CHECK-NEXT: call void @llvm.lifetime.end.p0(ptr{{.*}} %[[AGG1]])
   // CHECK-NEXT: call void @llvm.lifetime.start.p0(ptr{{.*}} %[[AGG2]])
-  // CHECK: invoke void @func_that_throws(ptr{{.*}} %[[AGG2]])
-  // CHECK-NEXT: to label %[[CONT2:.*]] unwind label %[[LPAD2:.*]]
+  // CHECK: invoke void @_func_that_throws(ptr{{.*}} %[[AGG2]])
+  // CHECK-NEXT: to label %[[CONT2:.*]] unwind label %[[LPAD]]
 
   // CHECK: [[CONT2]]:
-  // CHECK-DAG: call void @llvm.lifetime.end.p0(ptr{{.*}} %[[AGG2]])
-  // CHECK-DAG: call void @llvm.lifetime.end.p0(ptr{{.*}} %[[AGG1]])
+  // CHECK-NEXT: call void @llvm.lifetime.end.p0(ptr{{.*}} %[[AGG2]])
   // CHECK: br label %[[TRY_CONT:.*]]
 
-  // CHECK: [[LPAD1]]:
+  // CHECK: [[LPAD]]:
   // CHECK: landingpad
-  // CHECK: br label %[[EHCLEANUP:.*]]
+  // CHECK-NOT: call void @llvm.lifetime.end.p0(ptr{{.*}} %[[AGG1]])
+  // CHECK-NOT: call void @llvm.lifetime.end.p0(ptr{{.*}} %[[AGG2]])
+  // CHECK: br label %[[TRY_CONT]]
 
-  // CHECK: [[LPAD2]]:
-  // CHECK: landingpad
-  // CHECK: call void @llvm.lifetime.end.p0(ptr{{.*}} %[[AGG2]])
-  // CHECK: br label %[[EHCLEANUP]]
-
-  // CHECK: [[EHCLEANUP]]:
-  // CHECK: call void @llvm.lifetime.end.p0(ptr{{.*}} %[[AGG1]])
+  // CHECK: [[TRY_CONT]]:
+  // CHECK-NEXT: ret void
   try {
     func_that_throws(Trivial{0});
     func_that_throws(Trivial{0});
