4de4d1506adb3572df963c1b1d0144cda999f76a
Tony Tao <tonytao@ca.ibm.com>
2026-01-05 14:52:43 -0500

    undo commit fix

--- llvm/lib/Support/VirtualOutputBackends.cpp
+++ llvm/lib/Support/VirtualOutputBackends.cpp
@@ -288,7 +288,7 @@ Error OnDiskOutputFile::tryToCreateTemporary(std::optional<int> &FD) {
     SmallString<128> UniquePath;
     sys::fs::OpenFlags OF = generateFlagsFromConfig(Config);
     if (std::error_code EC =
-            sys::fs::createUniqueFile(ModelPath, NewFD, UniquePath))
+            sys::fs::createUniqueFile(ModelPath, NewFD, UniquePath, OF))
       return make_error<TempFileOutputError>(ModelPath, OutputPath, EC);
 
     if (Config.getDiscardOnSignal())
@@ -332,13 +332,7 @@ Error OnDiskOutputFile::initializeFile(std::optional<int> &FD) {
   // Not using a temporary file. Open the final output file.
   return createDirectoriesOnDemand(OutputPath, Config, [&]() -> Error {
     int NewFD;
-    sys::fs::OpenFlags OF = sys::fs::OF_None;
-    if (Config.getTextWithCRLF())
-      OF |= sys::fs::OF_TextWithCRLF;
-    else if (Config.getText())
-      OF |= sys::fs::OF_Text;
-    if (Config.getAppend())
-      OF |= sys::fs::OF_Append;
+    sys::fs::OpenFlags OF = generateFlagsFromConfig(Config);
     if (std::error_code EC = sys::fs::openFileForWrite(
             OutputPath, NewFD, sys::fs::CD_CreateAlways, OF))
       return convertToOutputError(OutputPath, EC);
